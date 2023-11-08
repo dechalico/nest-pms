a949986f82661287c556b9e3a83ac02fbad2e9e0
dechalico <dechalico.dev@gmail.com>
2023-11-08 23:27:08 +0800

    add openapi swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PMS Backend API')
+    .setDescription('Saviour PMS monitoring backend services')
+    .setVersion('1.0')
+    .addTag('PMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
