9a11f47ee0ca1c0fbe1f6e28e6a4d5aa5faa099b
dechalico <dechalico.dev@gmail.com>
2023-12-20 12:15:05 +0800

    add admin account services

---
+++ backend/src/services/adminServices/accountServices/services/profileHandler.ts
@@ -0,0 +1,28 @@
+import { AppResult, AppErrorCodes } from '../../../../common/app.result';
+import { IProfileHandler } from '../handlers/iProfileHandler';
+import { ProfileArgs, ProfileResult } from '../interactors/profileInteractor';
+import { ICurrentUserHandler } from '../../../authServices/handlers/ICurrentUserHandler';
+
+export class ProfileHandler implements IProfileHandler {
+  constructor(private readonly currentUserHandler: ICurrentUserHandler) {}
+
+  async executeAsync(args: ProfileArgs): Promise<AppResult<ProfileResult>> {
+    try {
+      const currentUserRes = await this.currentUserHandler.executeAsync({});
+      if (!currentUserRes.succeeded || !currentUserRes.result) {
+        return AppResult.createFailed(
+          new Error(currentUserRes.message),
+          currentUserRes.message,
+          currentUserRes.error.code,
+        );
+      }
+      return AppResult.createSucceeded(currentUserRes.result, 'Profile successfuly get');
+    } catch (error) {
+      return AppResult.createFailed(
+        error,
+        'An error occured in ProfileHandler',
+        AppErrorCodes.InternalError,
+      );
+    }
+  }
+}
