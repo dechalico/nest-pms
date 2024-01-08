169d72f004471f8ce14027e14e6f19620be4d0cf
dechalico <dechalico.dev@gmail.com>
2024-01-08 12:48:40 +0800

    set engineer page FE

---
+++ frontend/types/management/engineer.ts
@@ -0,0 +1,11 @@
+export interface Engineer 
+{
+    id: string;
+    dateCreated: Date;
+    dateUpdated?: Date;
+    firstName: string;
+    lastName: string;
+    middleName?: string;
+    areaOfficeId: string;
+    createdBy: string;
+}
