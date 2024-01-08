b28cbfab3cb481195e0150a98ef188e11629076b
dechalico <dechalico.dev@gmail.com>
2024-01-08 18:12:18 +0800

    integrate add engineer

--- frontend/pages/management/engineers.vue
+++ frontend/pages/management/engineers.vue
@@ -25,11 +25,29 @@
                     variant="text"
                   />
                 </template>
-                <v-form v-model="vFormModel" @submit.prevent="" validate-on="blur" class="pb-3">
+                <v-form
+                  v-model="vFormModel"
+                  @submit.prevent="createEngineerHandler"
+                  validate-on="blur"
+                  class="pb-3"
+                >
                   <div>
-                    <v-text-field label="First Name" variant="outlined" class="mb-5"></v-text-field>
-                    <v-text-field label="Last Name" variant="outlined" class="mb-5"></v-text-field>
                     <v-text-field
+                      v-model.trim="firstName.model"
+                      :rules="firstName.rules"
+                      label="First Name"
+                      variant="outlined"
+                      class="mb-5"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model.trim="lastName.model"
+                      :rules="lastName.rules"
+                      label="Last Name"
+                      variant="outlined"
+                      class="mb-5"
+                    ></v-text-field>
+                    <v-text-field
+                      v-model.trim="middleName.model"
                       label="Middle Name"
                       variant="outlined"
                       class="mb-5"
@@ -95,6 +113,7 @@
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import { PencilIcon, TrashIcon, XIcon } from 'vue-tabler-icons';
 import type { Engineer } from '@/types/management/engineer';
+import type { FormInput } from '@/types/pages/form';
 
 type EngineerResult = {
   engineers: Engineer[];
@@ -103,10 +122,44 @@ type EngineerResult = {
 const dialogModel = ref<boolean>(false);
 const vFormModel = ref<boolean>(false);
 
-const { data: engineerResult, refresh } = await useApiFetch<EngineerResult>(
+const firstName = reactive<FormInput<string>>({
+  model: '',
+  disabled: false,
+  rules: [(v: string) => !!v || 'First name field required.'],
+});
+const lastName = reactive<FormInput<string>>({
+  model: '',
+  disabled: false,
+  rules: [(v: string) => !!v || 'Last name field required.'],
+});
+const middleName = reactive<FormInput<string>>({
+  model: '',
+  disabled: false,
+});
+
+const { data: engineerResult, refresh: loadEngineers } = await useApiFetch<EngineerResult>(
   'admin/engineers/?includeOffice=true',
   {
     showError: true,
   },
 );
+
+const createEngineerHandler = async () => {
+  if (!vFormModel.value) return;
+
+  const payload = {
+    firstName: firstName.model,
+    lastName: lastName.model,
+    middleName: middleName.model,
+  };
+  const { status } = await useApiFetch('/admin/engineers', {
+    method: 'POST',
+    body: payload,
+    showError: true,
+  });
+  if (status.value === 'success') {
+    dialogModel.value = false;
+    await loadEngineers();
+  }
+};
 </script>
